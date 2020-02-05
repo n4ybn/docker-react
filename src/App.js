4e9f02738d093e98ca7d817957c81b01fc9e9a15
David DuPre <david.dupre@gmail.com>
2020-02-05 15:39:23 -0500

    text change

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React --- And phone home again and maybe not again twice!
+          Learn React --- And phone home again and maybe not again twice and 3rd.
         </a>
       </header>
     </div>
